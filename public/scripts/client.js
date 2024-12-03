facfb2067d95a0392b5e2b0c531e524ec794fcc6
Jaidenpearson <[jaidensrpearson@gmail.com]>
2024-12-02 21:16:49 -0800

    localhost no longer hard coded. Commented functions for better readability

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,23 +5,25 @@
  */
 $(document).ready(function(){
 
-  const timeFormat = timeago.format
+  const timeFormat = timeago.format //Formats timestamp on tweets
 
+  //Converts user input into text node to prevent injection
   const escape = function (str) {
     let div = document.createElement("div");
     div.appendChild(document.createTextNode(str));
     return div.innerHTML;
   };
 
+  //Displays red error on tweet form in case of error
   const displayError = (str) => {
     $('#tweet-error').css('display', 'inline').text(str)
   }
 
   const isTweetValid = () => {
     
-    const tweetText = $('#tweet-text').val().trim()
+    const tweetText = escape($('#tweet-text').val().trim())
 
-    if (tweetText === '' || tweetText === null) {
+    if (!tweetText) {
       displayError("Cannot post an empty tweet")
       return false
     } 
@@ -35,17 +37,18 @@ $(document).ready(function(){
     return true
   }
 
+//HTML format for a tweet
 const createTweetElement = function(tweet) {
 let $tweet =  $(`
   <article class="tweet">
         <header>
           <div class="left-header">
             <img src="${tweet.user.avatars}" alt="">
-            <p class="user-name">${escape(tweet.user.name)}</p>
+            <p class="user-name">${tweet.user.name}</p>
           </div>
-          <p class="user-handle">${escape(tweet.user.handle)}</p>
+          <p class="user-handle">${tweet.user.handle}</p>
         </header>
-        <h3>${escape(tweet.content.text)}</h3>
+        <h3>${tweet.content.text}</h3>
         <footer>
           <p>${timeFormat(tweet.created_at)}</p>
           <div class="icons">
@@ -60,6 +63,7 @@ let $tweet =  $(`
 return $tweet;
 }
 
+//Empties tweet container and reloads with all tweets
 const renderTweets = function(tweets) {
   $('.tweet-container').empty()
   for (const tweet of tweets) {
@@ -67,13 +71,15 @@ const renderTweets = function(tweets) {
   }
 }
 
+//Gets info from /tweets and calls renderTweets to reload page with all tweets
 const loadTweets = function() {
-  $.get('http://localhost:8080/tweets')
+  $.get('/tweets')
     .then(function(tweets) {
       renderTweets(tweets)
   })
 }
 
+//For button when scrolling away from nav bar. Returns to top of page and focuses on the tweet form
 $('#returnButton').on('click', function() {
   $('html, body').animate({
     scrollTop: $(".new-tweet").offset().top
@@ -91,6 +97,8 @@ $('#returnButton').on('click', function() {
   })
 })
 
+//Hides return button and slides down the nav bar when at the top of the page
+//Timeout is to debug strange animation behavior
 $('.returnButtonContainer').hide();
 
 let debounceTimer;
@@ -112,6 +120,8 @@ $(window).on('scroll', function() {
   }, 12); // Delay on Navbar slideDown
 });
 
+
+//Hides tweet form on load. Click 'Write a new tweet button' at top of page to access form
 $('.new-tweet').hide()
 
 $('.newTweetContainer').on('click', function() {
@@ -122,9 +132,10 @@ $('.newTweetContainer').on('click', function() {
       $('#tweet-text').focus()
     })
   }
-
 })
 
+
+//Submits form data to reload new tweet on page with all the other tweets
 $('.new-tweet-form').on('submit', function(event) {
   event.preventDefault()
 
@@ -141,7 +152,5 @@ $('.new-tweet-form').on('submit', function(event) {
       console.error(error)
     }))
   })
-
   loadTweets()
-
 });
